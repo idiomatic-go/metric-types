3289f5d3cbecb1fc451608ef6c72fa1d5e2461f7
idiomatic-go <113379405+idiomatic-go@users.noreply.github.com>
2022-09-29 11:40:00 -0500

    Refactoring

--- accesslogv3/tcpaccesslogentry.go
+++ accesslogv3/tcpaccesslogentry.go
@@ -1,12 +1,15 @@
 package accesslogv3
 
-import (
-	v31 "github.com/envoyproxy/go-control-plane/envoy/data/accesslog/v3"
-)
+type ConnectionProperties struct {
+	// Number of bytes received from downstream.
+	ReceivedBytes uint64 `protobuf:"varint,1,opt,name=received_bytes,json=receivedBytes,proto3" json:"received_bytes,omitempty"`
+	// Number of bytes sent to downstream.
+	SentBytes uint64 `protobuf:"varint,2,opt,name=sent_bytes,json=sentBytes,proto3" json:"sent_bytes,omitempty"`
+}
 
 type TCPAccessLogEntry struct {
 	// Common properties shared by all Envoy access logs.
 	//CommonProperties *Common
 	// Properties of the TCP connection.
-	ConnectionProperties *v31.ConnectionProperties
+	ConnectionProperties *ConnectionProperties
 }
